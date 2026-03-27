722671e58354fd8b97994199457612fac6ac6896
Will Donnelly <wsdonnelly@gmail.com>
2026-03-27 10:16:58 +0200

    [chore/updateGitWorkflow] chore(hooks) added branch name check on creation

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -8,4 +8,7 @@ chmod +x "$HOOKS_DIR/prepare-commit-msg"
 cp "$SCRIPTS_DIR/pre-push" "$HOOKS_DIR/pre-push"
 chmod +x "$HOOKS_DIR/pre-push"
 
+cp "$SCRIPTS_DIR/post-checkout" "$HOOKS_DIR/post-checkout"
+chmod +x "$HOOKS_DIR/post-checkout"
+
 echo "Git hooks installed."
